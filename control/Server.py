2f8d7be81be975919c931e128f45682049d28670
aga <da@example.com>
2024-03-14 18:34:39 -0400

    upd server

--- control/Server.py
+++ control/Server.py
@@ -129,9 +129,7 @@ class MainWindow:
 
         self.table.delete(*self.table.get_children())
 
-        if len(ids_modules[self.selected_sensor]['results']) > 0:
-            self.table['columns'] = [x for x in ids_modules[self.selected_sensor]['results'][0].keys()]
-            
+        self.table['columns'] = [x for x in ids_modules[self.selected_sensor]['results'][0].keys()]
         for col in self.table['columns']:
             self.table.heading(col, text=col)
 
