ee3991a076811bd8092f3cc8a5e090675e928ea1
aga <da@example.com>
2024-03-15 09:54:28 -0400

    add final logic for apache record builder

--- Apache/ApacheReportBuilder.py
+++ Apache/ApacheReportBuilder.py
@@ -1,6 +1,35 @@
 from logic.ReportBuilder import *
 
 class ApacheReportBuilder(ReportBuilder):
-
     def make_report(self, net_results):
-        return net_results
+        good_report = {}
+
+        rus = {'url': 'URL',
+               'ua' : 'User-Agent',
+               'params': 'POST параметры',
+               'method' : 'Метод',
+               'code': 'Статус код',
+               'predicted_class': 'Класс атаки'}
+        
+        attack_ignore = ['test', 'normal']
+
+        if 'results' in net_results:
+            good_report['results'] = []
+            records = net_results['results']
+            for record in records:
+                rus_record = {}
+                
+                for key in record:
+                    if key in rus.keys():
+                        rus_record[rus[key]] = record[key]
+
+                if rus_record['Класс атаки'] not in attack_ignore:
+                    good_report['results'].append(rus_record)
+
+        elif 'accuracy' in net_results:
+            good_report['accuracy'] = net_results['accuracy']
+
+        print(good_report)
+        return good_report
+    
+    
